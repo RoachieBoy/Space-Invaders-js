47be289490ee68bd522be0c8edbd96cad55c49c6
RoachieBoy <64902177+RoachieBoy@users.noreply.github.com>
2023-11-11 11:56:38 +0100

    Adding project files

---
+++ config.js
@@ -0,0 +1,4 @@
+// Main settings for the game screen
+const canvas_size = { width: 800, height: 600 };
+const background_colour = 0;
+
---
+++ sketch.js
@@ -0,0 +1,7 @@
+function setup() {
+createCanvas(canvas_size.width, canvas_size.height);
+}
+
+function draw() {
+  background(background_colour);
+}
